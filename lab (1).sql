c5822c9dedba1c4d0cfc97d8e58308b5ec8b5d10
Anonymous Coder <81471935+anonymouscoder7@users.noreply.github.com>
2022-01-21 09:01:07 +0545

    Add files via upload

---
+++ lab (1).sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 21, 2022 at 04:15 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+-- Error reading structure for table lab.admins: #1932 - Table &#039;lab.admins&#039; doesn&#039;t exist in engine
+-- Error reading data for table lab.admins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lab`.`admins`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+-- Error reading structure for table lab.contact: #1932 - Table &#039;lab.contact&#039; doesn&#039;t exist in engine
+-- Error reading data for table lab.contact: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lab`.`contact`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content`
+--
+-- Error reading structure for table lab.content: #1932 - Table &#039;lab.content&#039; doesn&#039;t exist in engine
+-- Error reading data for table lab.content: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lab`.`content`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+-- Error reading structure for table lab.gallery: #1932 - Table &#039;lab.gallery&#039; doesn&#039;t exist in engine
+-- Error reading data for table lab.gallery: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lab`.`gallery`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
